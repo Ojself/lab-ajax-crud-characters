714822dc364af1cdfccdba53fa34f785a78d8f09
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-02-06 12:31:48 +0100

    instructor solution

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,63 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
 
+
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+    $('input').val('');
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = function () {
+    const charID = $('input[name="character-id"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.getOneRegister(charID);
+    $('input').val('');
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault();
+    const data = getData('new');
+    if (data.name === '' || data.occupation === '' || data.weapon === '' || data.cartoon === '') alert('Empty ID field');
+    else charactersAPI.createOneRegister(data, e);
+    $('input').val('');
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    e.preventDefault();
+    const data = getData('edit');
+    const charID = $('input[name="chr-id"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.updateOneRegister(data, charID, e);
+    $('input').val('');
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('delete-one').onclick = function (e) {
+    const charID = $('input[name="character-id-delete"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.deleteOneRegister(charID, e);
+    $('input').val('');
   }
-})
+
+  const getData = (action) => {
+    let data = {
+      cartoon: $(`#${action}-character-form input[name="cartoon"]`).is(':checked')
+    };
+    if ($(`#${action}-character-form input[name="name"]`).val() !== '') {
+      data.name = $(`#${action}-character-form input[name="name"]`).val();
+    }
+    if ($(`#${action}-character-form input[name="occupation"]`).val() !== '') {
+      data.occupation = $(`#${action}-character-form input[name="occupation"]`).val();
+    }
+    if ($(`#${action}-character-form input[name="weapon"]`).val() !== '') {
+      data.weapon = $(`#${action}-character-form input[name="weapon"]`).val();
+    }
+    return data;
+  }
+
+
+
+
+
+
+});
